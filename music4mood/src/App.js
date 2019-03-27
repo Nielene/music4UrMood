aabf810e7e35b72b8eca11ed0bb3a528d5d3bdd6
Andres.C <andrescabrera@pursuit.org>
2019-03-27 17:22:21 -0400

    css stuff

--- music4mood/src/App.js
+++ music4mood/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import SingleSong from './components/singleSong.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <SingleSong />
-        </header>
+
       </div>
     );
   }
--- music4mood/src/components/singleSong.js
+++ music4mood/src/components/singleSong.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../css/singleSong.css";
 
 class SingleSong extends Component {
   render() {
     return (
-      <div className="=main">
+      <div className="main">
         <h4>Single Song will get going </h4>
+        <div class="vertical-menu">
+          <ul>
+            <li>
+              {" "}
+              <button>Happy</button>
+            </li>
+            <li>
+              {" "}
+              <button>Sad</button>
+            </li>
+            <li>
+              {" "}
+              <button>Romantic</button>
+            </li>
+            <li>
+              <button>Judy</button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
