e36028cbb06f712d2308f19bf8b06a1fb6af8d03
ThaiSonP <tysonpan@pursuit.org>
2019-03-27 15:04:11 -0400

    skeleton for playing a single song

---
+++ music4mood/src/playSong.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import axios from 'axios'
+
+class PlaySong extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  }
+  render(){
+    return(
+      <>
+        this is one song
+      </>
+    )
+  }
+}
+
+export default PlaySong
