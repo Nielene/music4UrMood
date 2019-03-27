536e7b8e4d7b4ad2d34ce8a8eba64051e74d2e62
Nielene Thomas <nielenethomas@pursuit.org>
2019-03-27 14:45:41 -0400

    test commit

---
+++ music4mood/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import NavbarHoriz from './NavbarHoriz'
+import NavbarVert from './NavbarVert'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
