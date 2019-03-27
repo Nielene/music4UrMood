5b665207ab8a87571664bc5fe275e6acf19a6ef2
Nicolle Loayza <n.loayza@gmail.com>
2019-03-27 17:17:28 -0400

    homepage n icons setup/css

--- music4mood/src/App.js
+++ music4mood/src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/app.css';
+
+let happy = require('./happy.gif');
+let sad = require('./sad.gif');
+let relax = require('./relax.gif');
+let love = require('./love.gif');
+let energy = require('./energetic.gif');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Music For Your Mood</h1>
+      <h1 className="welcome">Music For Your Mood</h1>
+
+      <div className='happy'>
+        <img src={happy} alt=''/>
+        <li className='happyT'>HAPPY</li>
+      </div>
+
+      <div className='sad'>
+        <img src={sad} alt=''/>
+        <li className='sadT'>EMO</li>
+      </div>
+
+      <div className='relax'>
+        <img src={relax} alt=''/>
+        <li className='relaxT'>RELAXED</li>
+      </div>
+
+      <div className='roman'>
+        <img src={love} alt='' />
+        <li className='romanT'>ROMANTIC</li>
+      </div>
+
+      <div className='energy'>
+        <img src={energy} alt=''/>
+        <li className='energyT'>HYPED</li>
+      </div>
+
       </div>
     );
   }
